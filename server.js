60add8af6b566124cd1647388b1764002aec44d0
Halmai McRae <hello@halmaimcrae.co.nz>
2023-07-14 12:30:46 +1200

    Integrate swagger

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import fastifyAutoload from '@fastify/autoload'
 import fastifyCors from 'fastify-cors' // Update the import statement
+import fastifySwagger from 'fastify-swagger'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 
@@ -9,10 +10,18 @@ const __dirname = dirname(__filename)
 export default async function (fastify, opts) {
   fastify.register(fastifyCors) // Register the fastify-cors plugin
 
+  fastify.register(fastifySwagger, {
+    routePrefix: '/docs',
+    exposeRoute: true,
+    swagger: {
+      info: {
+        title: 'API',
+      },
+    },
+  })
+
   fastify.register(fastifyAutoload, {
     dir: join(__dirname, 'routes'),
     options: { prefix: '/api' },
   })
 }
-
-// testing my commits
